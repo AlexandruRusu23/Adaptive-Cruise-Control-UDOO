0e76f990bacce1e32a609f88acf1b28ca66cc936
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2017-04-08 18:39:08 +0300

    server modified

--- sockets/serverSocket.py
+++ sockets/serverSocket.py
@@ -3,7 +3,7 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('192.168.0.104', 32654)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
@@ -18,17 +18,17 @@ while True:
     try:
         print >>sys.stderr, 'connection from', client_address
 
-        # Receive the data in small chunks and retransmit it
         while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
+            data = connection.recv(1024)
+            print >>sys.stderr, 'Client: "%s"' % data
             if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
+                input_var = input("Your answer: ")
+                print >>sys.stderr, 'You sent: %s ' % input_var
+                connection.sendall(input_var)
             else:
                 print >>sys.stderr, 'no more data from', client_address
                 break
-            
+
     finally:
         # Clean up the connection
         connection.close()
