bf5fea5aa4063f751070774a88823df998de25a0
AlexStahie <stahie.alexandru@gmail.com>
2017-04-08 18:50:41 +0300

    client fixed

--- sockets/clientSocket.py
+++ sockets/clientSocket.py
@@ -5,16 +5,11 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-<<<<<<< HEAD
 server_address = ('192.168.0.104', 32654)
-=======
-server_address = ('localhost', 32654)
->>>>>>> origin
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:
-<<<<<<< HEAD
     while True:
 	    # Send data
 	    message = input('Your message: ')
@@ -27,22 +22,6 @@ try:
 	    data = sock.recv(1024)
 	    
 	    print >>sys.stderr, 'received "%s"' % data
-=======
-
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
-
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
->>>>>>> origin
 
 finally:
     print >>sys.stderr, 'closing socket'
