a7f081e035dd884b82e064dc04015f318ae3d6a1
AlexStahie <stahie.alexandru@gmail.com>
2017-04-17 14:13:59 +0300

    jpeg conversion to optime the streaming rate

--- Python/clientStream.py
+++ Python/clientStream.py
@@ -8,6 +8,8 @@ cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('192.168.0.104', 8089))
 while True:
-    ret,frame=cap.read()
-    data = pickle.dumps(frame) ### new code
-    clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
+	ret,frame=cap.read()
+	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 60]
+	result, encimg = cv2.imencode('.jpg', frame, encode_param)
+	data = pickle.dumps(frame) ### new code
+	clientsocket.sendall(struct.pack("L", len(data))+data) ### new code
