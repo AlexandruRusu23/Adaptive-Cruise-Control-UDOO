027e263b8125d2d48ea1a463dc95f0278fc6db8b
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2017-05-18 02:58:31 +0300

    Revert "Revert "[All Components] Minor changes""
    This reverts commit 3ffe87520effcc094398560f0778a02a2e7b4e9d.

--- Arduino/ACCUdoo/ACCUdoo.ino
+++ Arduino/ACCUdoo/ACCUdoo.ino
@@ -48,11 +48,6 @@ void setup()
 void loop()
 {
   CommandManager();
-  if (millis() - print_timer > 500)
-  {
-    PrintCarData();
-    print_timer = millis();
-  }
 
   rightBackMotor->setSpeed(motorSpeedValue[RIGHT_BACK]);
   leftBackMotor->setSpeed(motorSpeedValue[LEFT_BACK]);
@@ -94,20 +89,6 @@ long getSerial()
     inByte = 0;
   }
   return serialData;
-}
-
-void PrintCarData()
-{
-  Serial.println("CAR_DATA");
-  Serial.print("SPEED: ");
-  if (turning == DISABLED)
-    Serial.println(normalSpeed);
-  else
-    Serial.println(directionSpeed);
-  Serial.print("ACTION: ");
-  Serial.println(action);
-  Serial.println("END_CAR_DATA");
-  Serial.println("");
 }
 
 void updateVectorSpeed()
